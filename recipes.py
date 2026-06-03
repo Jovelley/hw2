12e1eb0eecc632b87097371e390c47f457ea3df0
Jovelley <lovervarvara79@gmail.com>
2026-06-03 22:52:39 +0300

    Реализация класса ShoppingList

--- recipes.py
+++ recipes.py
@@ -57,4 +57,38 @@ class Recipe:
     def __str__(self):
         ingredients_str = "\n".join(str(i) for i in self.ingredients)
         return f"{self.title}:\n{ingredients_str}"
-    
+
+
+class ShoppingList:
+    def __init__(self):
+        self._items = []
+
+    def add_recipe(self, recipe: Recipe, portions: float):
+        if portions <= 0:
+            raise ValueError("Количество порций должно быть положительным")
+        scaled_recipe = recipe.scale(portions)
+        for ingredient in scaled_recipe.ingredients:
+            self._items.append((ingredient, recipe.title))
+
+    def remove_recipe(self, title):
+        self._items = [item for item in self._items if item[1] != title]
+
+    def get_list(self):
+        result = {}
+        for ingredient, _ in self._items:
+            key = (ingredient.name, ingredient.unit)
+            if key in result:
+                result[key] += ingredient.quantity
+            else:
+                result[key] = ingredient.quantity
+        ingredients = [
+            Ingredient(name, quantity, unit)
+            for (name, unit), quantity in result.items()
+        ]
+        ingredients.sort(key=lambda x: x.name)
+        return ingredients
+
+    def __add__(self, other):
+        new_list = ShoppingList()
+        new_list._items = self._items + other._items
+        return new_list
